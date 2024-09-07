a951476781447d1b566c1451aebaf3d0172e36d0
Vladimir <duenasvladimir@gmail.com>
2024-09-06 19:41:22 -0600

    Add profile page with basic styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
+import 'profile.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+const primaryColor = Color.fromARGB(255, 207, 207, 207);
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() => runApp(
+  MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+    primaryColor: primaryColor,
+   ),
+    initialRoute: '/',
+    routes: {
+     '/': (context) => HomeScreen(),
+      '/profile': (context) => UsrProfile(),
+    },
+  ),
+);
 
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Hello World!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Menu'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/profile');
+            },
+            child: Text('User profile'),
+          ),
         ),
       ),
     );
   }
 }
+
