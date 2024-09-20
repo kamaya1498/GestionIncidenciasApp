6b5d578f4824377fa0189f3a8a68798577d2b8ca
Yoeli Ismael Guzman <yoeli.exe@gmail.com>
2024-09-20 09:57:31 -0600

    Reestructura del proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,103 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
-import 'lista_incidencias.dart'; // Importa la pantalla de lista de incidencias
+import 'crear_incidencia.dart';  // Importar el archivo para la pantalla de Crear Incidencia
+import 'reporte.dart';          // Importar el archivo para la pantalla de Reporte
+import 'perfil.dart';           // Importar el archivo para la pantalla de Perfil
+import 'main.dart'; 
+import 'home.dart';             // Importar el archivo para redirigir al login (main)
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(), // Define la pantalla principal como HomeScreen
+      debugShowCheckedModeBanner: false,
+      title: 'Registro de Incidencias',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),  // Pantalla inicial
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navega a la pantalla de lista de incidencias
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const ListaIncidencias(),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.contain,
+                ),
               ),
-            );
-          },
-          child: const Text('Ver Incidencias'),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Aquí hacemos la redirección a la pantalla Home
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),  // Redirigir a HomeScreen
+                  );
+                },
+                child: Text(
+                  'Iniciar sesión',
+                  style: TextStyle(
+                    color: Colors.white,  // Cambia el color del texto a blanco (#ffffff)
+                    fontWeight: FontWeight.bold,  // Texto en negrita
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 32, 69, 99),  // Color de fondo del botón
+                  minimumSize: Size(double.infinity, 50),  // El botón ocupa todo el ancho
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: double.infinity,  // El footer ocupa todo el ancho de la pantalla
+                color: Color(0xFFE2E2E2),  // Fondo tipo footer en color #E2E2E2
+                padding: EdgeInsets.all(15.0),  // Padding de 15px en todos los lados
+                child: GestureDetector(
+                  onTap: () {
+                    // Acción para recuperar contraseña
+                  },
+                  child: Text(
+                    '¿Olvidó su contraseña? Recupérela.',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                    textAlign: TextAlign.center,  // Texto centrado
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/menu_widget.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'crear_incidencia.dart';  // Importar el archivo para la pantalla de Crear Incidencia
+import 'reporte.dart';          // Importar el archivo para la pantalla de Reporte
+import 'perfil.dart';           // Importar el archivo para la pantalla de Perfil
+import 'main.dart';
+import 'home.dart';             // Importar el archivo para redirigir al login (main)
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            height: 100, // Altura reducida para el DrawerHeader
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,  // Color de fondo para el DrawerHeader
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'NombreUsuario/a',  // Mostrar el nombre del usuario
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      icon: Icon(Icons.close, color: Colors.white),  // Botón de cerrar con icono 'X'
+                      onPressed: () {
+                        Navigator.pop(context);  // Cierra el Drawer
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Inicio'),
+            onTap: () {
+              Navigator.pop(context);  // Cierra el drawer
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),  // Redirige a la pantalla Home
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.add_circle_outline),
+            title: Text('Crear Incidencia'),
+            onTap: () {
+              Navigator.pop(context);  // Cierra el drawer
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CrearIncidenciaScreen()),  // Redirige a la pantalla Crear Incidencia
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.report),
+            title: Text('Ver Reporte'),
+            onTap: () {
+              Navigator.pop(context);  // Cierra el drawer
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ReporteScreen()),  // Redirige a la pantalla Reporte
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Perfil'),
+            onTap: () {
+              Navigator.pop(context);  // Cierra el drawer
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PerfilScreen()),  // Redirige a la pantalla Perfil
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Cerrar sesión'),
+            onTap: () {
+              Navigator.pop(context);  // Cierra el drawer
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),  // Redirige a la pantalla Main (Login)
+                (Route<dynamic> route) => false,  // Elimina todas las rutas anteriores para evitar volver
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
