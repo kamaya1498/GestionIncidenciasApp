3f41972fee4e1f7610531566c4dd9beccf51d20c
raymondxx23 <1714322020@mail.utec.edu.sv>
2024-09-22 07:28:16 -0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,26 +1,105 @@
 import 'package:flutter/material.dart';
-import 'menu_widget.dart';  // Importar el Drawer personalizado
+import 'package:home/CrearIncidencia.dart';
+import 'package:home/reporte.dart';
 
-class HomeScreen extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  get width => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();  // Abre el menú de hamburguesa
-              },
-            );
-          },
+        backgroundColor: const Color.fromARGB(255, 0, 47, 79),
+        foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text('NombreUsuario'),
+          ],
         ),
       ),
-      drawer: CustomDrawer(),  // Usar el widget Drawer personalizado
       body: Center(
-        child: Text('Bienvenido a la pantalla de inicio'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Bienvenido/a',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Image.asset(
+              'assets/home.png',
+              width: 250, // Ajusta el ancho de la imagen
+              height: 250, // Ajusta la altura de la imagen
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => CrearIncidenciaScreen()),
+                );
+              },
+              child: Text('Registrar Incidencia'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                backgroundColor:
+                    const Color.fromARGB(255, 0, 55, 89), // Color del texto
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ReporteScreen()),
+                );
+              },
+              child: Text('Reporte'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                backgroundColor: const Color.fromARGB(255, 85, 93, 98),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            Spacer(),
+            Text('¿Tiene problemas técnicos?'),
+            GestureDetector(
+              onTap: () {},
+              child: Text(
+                'Reportelo',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
